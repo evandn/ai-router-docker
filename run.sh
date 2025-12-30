00bf86e9c3babfb029bca96f92d5dfcfe0a6736e
Evan Duong <evduong@icloud.com>
2025-12-30 22:15:33 +0700

    Update `cli-proxy-api` base image and add configurable config file handling to `run.sh`

--- run.sh
+++ run.sh
@@ -3,5 +3,12 @@
 # Run Tailscale in background
 KUBERNETES_SERVICE_HOST='' /usr/local/bin/containerboot &
 
+# Use custom config if provided, otherwise fall back to default
+DEFAULT_CONFIG_FILE='/app/config.yaml'
+CONFIG_FILE="${CONFIG_FILE:-$DEFAULT_CONFIG_FILE}"
+
+# Use default config if custom config doesn't exist
+[ -f "$CONFIG_FILE" ] || cp "$DEFAULT_CONFIG_FILE" "$CONFIG_FILE"
+
 # Run in foreground as PID 1
-exec ./CLIProxyAPI
+exec ./CLIProxyAPI --config "$CONFIG_FILE"
