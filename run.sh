15c8a693c6914d0912452972c0fce46904e0bab6
Evan Duong <evduong@icloud.com>
2025-12-31 13:10:03 +0700

    Bump `cli-proxy-api` to v6.6.73

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 # Run Tailscale in background
 KUBERNETES_SERVICE_HOST='' /usr/local/bin/containerboot &
 
-# Use custom config if provided, otherwise fall back to default
+# Use custom config file if provided, otherwise use default
 DEFAULT_CONFIG_FILE='/app/config.yaml'
 CONFIG_FILE="${CONFIG_FILE:-$DEFAULT_CONFIG_FILE}"
 
-# Use default config if custom config doesn't exist
+# Clone default config if custom config doesn't exist
 [ -f "$CONFIG_FILE" ] || cp "$DEFAULT_CONFIG_FILE" "$CONFIG_FILE"
 
 # Run in foreground as PID 1
